b5a397eea25b1b093657cb90f93b03c7ad88d403
vladimir <lovkiy2012@gmail.com>
2020-02-27 17:33:29 +0300

    3

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,8 @@
-import * as Font from "expo-font";
-export async function bootstarap() {
+import * as Font from 'expo-font'
+
+export async function bootstrap() {
   await Font.loadAsync({
-    "open-bold": require("../assets/OpenSans-Bold.ttf"),
-    "open-regular": require("../assets/OpenSans-Regular.ttf")
-  });
+    'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+    'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
+  })
 }
