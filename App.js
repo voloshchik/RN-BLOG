23a3efa2fa40e1f74eafc5bef31e23444a201fce
vladimir <lovkiy2012@gmail.com>
2020-02-27 17:54:26 +0300

    4

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { AppLoading } from "expo";
-import { bootstrap } from "./src/bootstrap";
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { AppLoading } from 'expo'
+import { AppNavigation } from './src/navigation/AppNavigation'
+import { bootstrap } from './src/bootstrap'
 
 export default function App() {
-  const [isReady, setIsReadi] = useState(false);
+  const [isReady, setIsReady] = useState(false)
+
   if (!isReady) {
     return (
       <AppLoading
         startAsync={bootstrap}
-        onFinish={() => setIsReadi(true)}
+        onFinish={() => setIsReady(true)}
         onError={err => console.log(err)}
       />
-    );
+    )
   }
-  return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      <Text style={{ justifyContent: "center", alignItems: "center" }}>
-        Open up App.jskkkkkkkkkkkkkkk to start working on your app!
-      </Text>
-    </View>
-  );
+
+  return <AppNavigation />
 }
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,18 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { MainScreen } from '../screens/MainScreen'
+import { PostScreen } from '../screens/PostScreen'
+
+const PostNavigator = createStackNavigator(
+  {
+    Main: MainScreen,
+    Post: {
+      screen: PostScreen
+    }
+  },
+  {
+    initialRouteName: 'Main'
+  }
+)
+
+export const AppNavigation = createAppContainer(PostNavigator)
