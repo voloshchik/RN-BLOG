39007dd825905bb4ba1377e910941e97edd3007a
vladimir <lovkiy2012@gmail.com>
2020-02-27 17:36:34 +0300

    3.1

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AppLoading } from "expo";
-import { bootstarap } from "./src/bootstrap";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
   const [isReady, setIsReadi] = useState(false);
   if (!isReady) {
     return (
       <AppLoading
-        startAsync={bootstarap}
+        startAsync={bootstrap}
         onFinish={() => setIsReadi(true)}
         onError={err => console.log(err)}
       />
