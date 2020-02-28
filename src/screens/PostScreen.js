c4b9ce66b28d1684e615ecb50b7f207661cdbc1d
vladimir <lovkiy2012@gmail.com>
2020-02-28 17:44:56 +0300

    71 style PostScreen

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,18 +1,56 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+  Alert
+} from "react-native";
+import { DATA } from "../data";
+import { THEME } from "../../theme";
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam("postId");
+  const post = DATA.find(p => p.id === postId);
+  const removeHadler = () => {
+    Alert.alert(
+      "Удаление поста",
+      "Вы действительно хотите удалить пост ?",
+      [
+        {
+          text: "Отмена",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "Удалить",
+          style: "destructive",
+          onPress: () => console.log("OK Pressed")
+        }
+      ],
+      { cancelable: false }
+    );
+  };
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView style={styles.center}>
+      <Image style={styles.image} source={{ uri: post.img }} />
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title="Удалить"
+        color={THEME.DANGER_COLOR}
+        onPress={removeHadler}
+      />
+    </ScrollView>
   );
 };
-PostScreen.navigationOptions = ({navigation}) => {
-  const date = navigation.getParam('date');
+PostScreen.navigationOptions = ({ navigation }) => {
+  const date = navigation.getParam("date");
   return {
-    headerTitle: "Пост от " + new Date(date).toLocaleDateString() ,
+    headerTitle: "Пост от " + new Date(date).toLocaleDateString(),
     headerStyle: {
       backgroundColor: "red",
       headerTintcolor: "#fff"
@@ -20,9 +58,14 @@ PostScreen.navigationOptions = ({navigation}) => {
   };
 };
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  image: {
+    width: "100%",
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  title: {
+    fontFamily: "open-regular"
   }
 });
