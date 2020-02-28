45af6e76ef53dae742d162c79dd4d9005c3dc94c
vladimir <lovkiy2012@gmail.com>
2020-02-28 21:48:43 +0300

    76 android start

--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,18 +1,55 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-export const BookedScreen = ({}) => {
+import Post from "../componens/Post";
+import { DATA } from "../data";
+import AppHeaderIcon from "../componens/AppHeaderIcon";
+
+export const BookedScreen = ({ navigation }) => {
+  const openPostHandler = post => {
+    navigation.navigate("Post", {
+      postId: post.id,
+      date: post.date,
+      booked: post.booked
+    });
+  };
   return (
-    <View style={styles.center}>
-      <Text>BookedScreen</Text>
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA.filter(post => post.booked)}
+        keyExtractor={post => post.id.toString()}
+        renderItem={({ item }) => {
+          return <Post post={item} onOpen={openPostHandler} />;
+        }}
+      />
     </View>
+  );
+};
+BookedScreen.navigationOptions = {
+  headerTitle: "Мой блог",
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="take photo"
+        iconName="ios-camera"
+        onPress={() => console.log("ssss")}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title="Toggle Drawer"
+        iconName="ios-menu"
+        onPress={() => console.log("ssss")}
+      />
+    </HeaderButtons>
   )
-}
-
+};
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  wrapper: {
+    padding: 10
   }
-})
+});
